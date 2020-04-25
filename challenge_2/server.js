b5e6dd0522982b318d92e2421f320b0c400b0f47
Imran Kheraj <ikheraj17@gmail.com>
2020-04-25 10:17:23 -0500

    server running

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
-app.use(express.static(__dirname + './client';
+app.use(express.static(__dirname + './client'));
 app.use(bodyParser.text ());
 app.use(bodyParser.urlencoded({extended : true}));
 app.listen(process.env.port || port, () => {
