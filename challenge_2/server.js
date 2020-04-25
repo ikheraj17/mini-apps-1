e54074f84440cba489094ae1e6659a49bdc09219
Imran Kheraj <ikheraj17@gmail.com>
2020-04-25 10:12:43 -0500

    npm installed express, body-parser, nodemon. Basic express code written

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+app.use(express.static(__dirname + './client';
+app.use(bodyParser.text ());
+app.use(bodyParser.urlencoded({extended : true}));
+app.listen(process.env.port || port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+})
 
 
