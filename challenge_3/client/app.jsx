9fb09a70b74f61b69a3d1a9f215026f8af3e136d
Imran Kheraj <ikheraj17@gmail.com>
2020-04-25 15:00:04 -0500

    Was working in challenge 2 by accident... reinstalled all dependencies, react and babel working well, rendering to DOM and displaying in browser.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,19 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            test : 'yoohoo'
+        }
+    }
 
+    render () {
+        return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,38 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+app.use(express.static('public'));
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended : true}));
+app.listen(process.env.port || port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fetcher', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
+let userSchema = mongoose.Schema({
+    Name: String,
+    Email: {type : String, unique : true},
+    Password: String,
+    Address1: String,
+    Address2: String,
+    City: String,
+    State: String,
+    Zip: Number,
+    Phone: Number,
+    CC: Number,
+    Expiry: String,
+    CVV: Number,
+    Bzip: Number
+});
+
+let User = mongoose.model('User', userSchema, 'shopper');
+
+//-------------------------------------------------//
+//---------------- ROUTES --------------------------//
+//-----------------------------------------------//
+
+// app.post("/api/users", (req, res) => {
+// });
 
